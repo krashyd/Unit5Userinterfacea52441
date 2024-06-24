5a5128b331a8ebcf586cd0a0870857f974cb8f05
krashyd <a52441@alunos.ipb.pt>
2024-06-24 12:35:59 +0100

    Lesson 5.3 - Game Over
    a52441

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -31,13 +31,22 @@ public class Target : MonoBehaviour
 
      private void OnMouseDown()
     {
+        if(gameManager.isGameActive)
+        {
         Destroy(gameObject);
         Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
         gameManager.UpdateScore(pointValue);
+        }
+        
     }
     private void OntriggerEnter(Collider other)
     {
         Destroy(gameObject);
+
+        if(!gameObject.CompareTag("Bad"))
+         {
+             gameManager.GameOver(); 
+         }
     }
 
     Vector3 RandomForce()
